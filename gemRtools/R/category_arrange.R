3e845e8a79f07cabe8128eaf9fc5101c45882220
Sunmin Lee <sunminlee@Sunmins-MacBook-Pro.local>
2021-05-20 01:36:28 -0400

    category arrange function

---
+++ gemRtools/R/category_arrange.R
@@ -0,0 +1,26 @@
+#' A Category Arrange Helper Function
+#'
+#' This function takes a vector of categories and rearranges them in alphabetical order
+#' ver. May 20 2021
+#' @param category Vector of categories to be arranged.
+#' @keywords wide
+#' @export
+#' @examples
+#' category_arrange(category)
+
+library(stringr)
+library(purrr)
+
+
+category <- c("ethnicity", "location", "location & ethnicity", "location & religion",
+              "location & sex", "location & sex & wealth", "location & wealth",
+              "region", "religion", "sex", "sex & ethnicity", "sex & region",
+              "sex & religion", "sex & wealth", "sex & wealth & region", "total",
+              "wealth", "wealth & region")
+
+
+# arrange categories by alphabetical order and convert first letter into uppercase
+category_arrange <- function(category) {
+  str_split(category, " & ") %>% map(sort) %>% map_chr(paste, collapse = " & ") %>% str_to_title()
+}
+
--- gemRtools/R/val_function.R
+++ gemRtools/R/val_function.R
@@ -12,7 +12,6 @@ df <- read.csv(file = '/Users/sunminlee/Desktop/gemr/gemRpackage/microdata/DHS_C
 View(df)
 
 
-# super working
 val_function <- function(df, var) {
   # condition 1: check negative values
   for (i in 1:nrow(df)){
@@ -26,99 +25,3 @@ val_function <- function(df, var) {
 }
 
 
-
-
-
-
-######## BELOW TESTING CODE - TO BE REMOVED ###########
-read_function <- function(survey, iso_code3, year) {
-  survey <- deparse(substitute(survey))
-  iso_code3 <- deparse(substitute(iso_code3))
-  year <- deparse(substitute(year))
-
-  # File name from function
-  f <- paste0(survey, "_", iso_code3, "_", year, ".csv")
-
-  # Open the dataset that matches the file name
-  for (f_path in list.files(path=".", all.files=FALSE)) {
-    if (f == f_path) {
-      print("Open dataset!")
-      micro_data <- read.csv(f)
-      View(micro_data)
-      break
-    } else {
-      print("No dataset. Please check the working directory!")
-      break
-    }
-  }
-
-}
-
-
-
-# test
-dataframe <- data.frame(A=1:10, B=2:11, C=3:12)
-fun1 <- function(x, column){
-  max(x[,column])
-}
-fun1(dataframe, "B")
-fun1(dataframe, c("B","A"))
-
-
-val_function <- function(variable) {
-  #var <- toString(variable)
-  var <- deparse(substitute(variable))
-  #var <- enquo(variable)
-  print(var)
-  # condition 1: check negative values
-  for (i in 1:nrow(df)){
-    if (df$var[i] >= 0){
-      print("positive")
-    } else {
-      print("negative")
-    }
-  }
-}
-
-
-for(i in 1:nrow(d)) {
-  if(d$value[i] %% 2 == 0){
-    d$value[i] <-0
-  }
-}
-d
-}
-
-
-
-
-survey <- deparse(substitute(survey))
-
-library(dplyr)
-my_summarise <- function(df, group_var) {
-  group_var <- enquo(group_var)
-  print(group_var)
-  df %>%
-    group_by(!! group_var) %>%
-    summarise(a = mean(a))
-}
-my_summarise(df, g1)
-
-
-# this is working
-for (i in 1:nrow(df)){
-  if (df$age[i] < 0) {
-    print("yes")
-  } else {
-    print("no")
-  }
-}
-
-
-# this is working
-val_function <- function(variable) {
-  # condition 1: check negative values
-  for (i in 1:nrow(df)){
-    with(df, df$variable[i] <= 0)
-  }
-}
